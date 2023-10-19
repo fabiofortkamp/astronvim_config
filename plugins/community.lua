9ca9dbd2dee85642d1aae344281183397a28a47e
Fábio Fortkamp <fabio@fabiofortkamp.com>
2023-10-19 12:35:38 -0300

    Configure smatcolumn.

--- plugins/community.lua
+++ plugins/community.lua
@@ -34,7 +34,10 @@ return {
   {
     "m4xshen/smartcolumn.nvim",
     opts = {
-      colorcolumn = 88,
+      colorcolumn = "80",
+      disabled_filetypes = { "help", "text", "markdown" },
+      custom_colorcolumn = {},
+      scope = "file",
     },
   },
 }
