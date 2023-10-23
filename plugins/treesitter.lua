436a62301800ef1a709e64075f04a30f5df849c7
Fábio P. Fortkamp <fabio@fabiofortkamp.com>
2023-10-23 13:25:34 -0300

    Add awk LSP and Treesitter

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -4,12 +4,13 @@ return {
     -- add more things to the ensure_installed table protecting against community packs modifying it
     opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
       "lua",
-"python",
-"julia",
-"jq",
-"ruby",
-"json",
-"bash"
+      "python",
+      "julia",
+      "jq",
+      "ruby",
+      "json",
+      "awk",
+      "bash",
     })
   end,
 }
