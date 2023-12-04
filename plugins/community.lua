6a68e53c03168faf01fceb4d6b8926398797a1fd
Fábio Fortkamp <fabio@fabiofortkamp.com>
2023-12-04 07:14:32 -0300

    Add clojure pack

--- plugins/community.lua
+++ plugins/community.lua
@@ -16,6 +16,7 @@ return {
   { import = "astrocommunity.pack.cpp" },
   { import = "astrocommunity.pack.go" },
   { import = "astrocommunity.pack.java" },
+  { import = "astrocommunity.pack.clojure" },
   { import = "astrocommunity.completion.copilot-lua-cmp" },
   { -- further customize the options set by the community
     "zbirenbaum/copilot.lua",
