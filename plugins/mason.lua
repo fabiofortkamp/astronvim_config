4e4e32dbc9c0f7524e9f37497b406e10d4c42e39
Fábio P. Fortkamp <fabio@fabiofortkamp.com>
2024-02-06 13:19:15 -0300

    Add Docker configuration

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -9,6 +9,8 @@ return {
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
         "lua_ls",
         "awk_ls",
+        "dockerls",
+        "docker_compose_language_service",
       })
     end,
   },
